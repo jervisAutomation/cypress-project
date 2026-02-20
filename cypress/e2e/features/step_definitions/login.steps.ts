9bcf4d64e397e6410c814f06340c0ef7020cf7c6
niceact00 <jervis.socmedandgames110@gmail.com>
2026-02-20 12:40:54 +0800

    added global hooks and updated package.json

--- cypress/e2e/features/step_definitions/login.steps.ts
+++ cypress/e2e/features/step_definitions/login.steps.ts
@@ -1,15 +1,6 @@
 import { Given, When, Then, Before } from "@badeball/cypress-cucumber-preprocessor";
 import { loginPage } from "cypress/support/pageInstances";
 
-Before({ tags: "@requiresLogin" }, () => {
-  cy.visit("/");
-
-  cy.fixture("loginData").then((data) => {
-    const credentials = data.validUser;
-    loginPage.login(credentials.username,credentials.password);
-  });
-});
-
 Then("I should be on the {string} page", (pageName: string) =>{
 cy.contains(pageName);
 })
